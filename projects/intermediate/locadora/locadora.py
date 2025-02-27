ec54c5a7689ef10bdc76e3044e1c63e52b92cfdb
jpsantosz <jpsantosc1503@gmail.com>
2025-02-26 21:10:11 -0300

    projeto videolocadora

--- projects/intermediate/locadora/locadora.py
+++ projects/intermediate/locadora/locadora.py
@@ -1,3 +1,5 @@
+import json
+
 class Filme:
     def __init__(self, id_filme: int, titulo: str, genero: str):
         self.id_filme = id_filme
@@ -6,7 +8,7 @@ class Filme:
         self.disponivel = True
         
 class Cliente:
-    def __init__(self, id_cliente: str, nome: str):
+    def __init__(self, id_cliente: int, nome: str):
         self.id_cliente = id_cliente
         self.nome = nome
         
@@ -25,23 +27,72 @@ class Locadora:
         
     def adicionar_cliente(self, cliente: Cliente):
         self.clientes.append(cliente)
-        print(f"O filme {cliente.nome} foi adicionado ao sistema.")
+        print(f"O cliente {cliente.nome} foi adicionado ao sistema.")
         self.salvar_dados()
         
-    def excluir_filme(self, filme: Filme):
+    def excluir_filme(self, id_filme):
+        filme = next((f for f in self.filmes if f.id_filme == id_filme), None)
         self.filmes.remove(filme)
         print(f"O filme {filme.titulo} foi excluído do sistema.")
         self.salvar_dados()
         
-    def excluir_cliente(self, cliente: Cliente):
-        self.filmes.remove(cliente)
+    def excluir_cliente(self, id_cliente: int):
+        cliente = next((c for c in self.clientes if c.id_cliente == id_cliente), None)
+        self.clientes.remove(cliente)
         print(f"O cliente {cliente.nome} foi excluído do sistema.")
         self.salvar_dados()
         
     def listar_filmes(self):
         for filme in self.filmes:
-            print(f"{filme.nome} - {filme.id_filme}")
+            print(f"{filme.titulo} - {filme.id_filme}")
     
     def listar_clientes(self):
         for cliente in self.clientes:
             print(f"{cliente.nome} - {cliente.id_cliente}")
+    
+    def alugar_filme(self, id_filme: int, id_cliente: int):
+        filme = next((f for f in self.filmes if f.id_filme == id_filme and f.disponivel), None)
+        cliente = next((c for c in self.clientes if c.id_cliente == id_cliente), None)
+        
+        if filme and cliente:
+            filme.disponivel = False
+            self.emprestimos.setdefault(id_cliente, []).append(id_filme)
+            self.salvar_dados()
+            print(f"O filme {filme.titulo} foi alugador por {cliente.nome}.")
+        else:
+            print("Filme ou cliente não encontrado!")
+            
+    def devolver_filme(self, id_filme: int, id_cliente: int):
+        if id_cliente in self.emprestimos and id_filme in self.emprestimos[id_cliente]:
+            filme = next((f for f in self.filmes if f.id_filme == id_filme), None)
+            filme.disponivel = True
+            self.emprestimos[id_cliente].remove(id_filme)
+            if not self.emprestimos[id_cliente]:
+                del self.emprestimos[id_cliente]
+            self.salvar_dados()
+            print(f"O filme {filme.titulo} foi devolvido.")
+        else:
+            print("Filme ou cliente não encontrado!")
+            
+    def salvar_dados(self):
+        data = {
+            "filmes":  [{"Título": f.titulo, "Gênero": f.genero, "Id": f.id_filme, "disponivel": f.disponivel} for f in self.filmes],
+            "clientes":  [{"Nome": c.nome, "Id": c.id_cliente} for c in self.clientes],
+            "Empréstimos":  self.emprestimos 
+        }
+        
+        with open (self.arquivo_dados, "w") as file:
+            json.dump(data, file, indent=4)
+            
+        
+    def carregar_dados(self):
+        try:
+            with open (self.arquivo_dados, "r") as file:
+                data = json.load(file)
+                self.filmes = [Filme(f["Id"], f["Título"], f["Gênero"]) for f in data["filmes"]]
+                for filme, disponivel in zip(self.filmes, [f["disponivel"] for f in data["filmes"]]):
+                    filme.disponivel = disponivel
+                self.clientes = [Cliente(c["Id"], c["Nome"]) for c in data["clientes"]]
+                self.emprestimos = data["Empréstimos"]
+        except FileNotFoundError:
+            self.salvar_dados()  
--- projects/intermediate/locadora/locadora_app.py
+++ projects/intermediate/locadora/locadora_app.py
@@ -1,1 +1,67 @@
 from locadora import Locadora, Cliente, Filme
+
+locadora = Locadora()
+
+while True:
+    print("\n📚 MENU Locadora")
+    print("1. Adicionar Filme")
+    print("2. Adicionar Cliente")
+    print("3. Alugar Filme")
+    print("4. Devolver Filme")
+    print("5. Listar Filmes")
+    print("6. Listar Clientes")
+    print("7. Excluir Filme")
+    print("8. Excluir Cliente")
+    print("0. Sair")
+    
+    escolha = input("Escolha uma ação: ")
+    
+    if escolha == "1":
+        titulo = input("Digite o nome do filme: ")
+        id = int(input("Digite o ID do filme: "))
+        genero = input("Digite o gênero do filme: ")
+        filme = Filme(id, titulo, genero)
+        locadora.adicionar_filme(filme)
+        print(f"O filme {filme.titulo} foi adicionado ao catálogo.")
+    
+    elif escolha == "2":
+        nome = input("Digite o nome do cliente: ")
+        id = int(input("Digite o ID do cliente: "))
+        cliente = Cliente(id, nome)
+        locadora.adicionar_cliente(cliente)
+        print(f"O cliente {cliente.nome} foi adicionado a lista de clientes.")
+    
+    elif escolha == "3":
+        id_filme = int(input("Digite o ID do filme: "))
+        id_cliente = int(input("Digite o ID do cliente: "))
+        locadora.alugar_filme(id_filme, id_cliente)
+        
+    elif escolha == "4":
+        id_filme = int(input("Digite o ID do filme: "))
+        id_cliente = int(input("Digite o ID do cliente: "))
+        locadora.devolver_filme(id_filme, id_cliente)
+        
+    elif escolha == "5":
+        print("Catálogo: \n")
+        locadora.listar_filmes()
+        
+    elif escolha == "6":
+        print("Lista de clientes: \n")
+        locadora.listar_clientes()
+        
+    elif escolha == "7":
+        id = int(input("Digite o ID do filme: "))
+        locadora.excluir_filme(id)
+        print("Filme excluído com sucesso")
+        
+    elif escolha == "8":
+        id = int(input("Digite o ID do cliente: "))
+        locadora.excluir_cliente(id)
+        print("Cliente excluído com sucesso")
+        
+    elif escolha == "0":
+        print("Encerrando o programa...")
+        break
+    
+    else:
+        print("Por favor selecione uma opção válida!")
